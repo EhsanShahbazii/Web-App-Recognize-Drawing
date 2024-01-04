b70231e02c1f3c9bd26f6207a94fa9be4a75d0e2
EhsanShahbazii <ehsan.shahbazipc@gmail.com>
2024-01-04 23:15:37 +0330

    add undo button and function for it

--- web/sketchPad.js
+++ web/sketchPad.js
@@ -9,10 +9,18 @@ class SketchPad {
     `;
     container.appendChild(this.canvas);
 
+    const lineBreak = document.createElement("br");
+    container.appendChild(lineBreak);
+
+    this.undoBtn = document.createElement("button");
+    this.undoBtn.innerHTML = "UNDO";
+    container.appendChild(this.undoBtn);
+
     this.ctx = this.canvas.getContext("2d");
 
     this.paths = [];
     this.isDrawing = false;
+    this.#redraw();
 
     this.#addEventListeners();
   }
@@ -50,11 +58,21 @@ class SketchPad {
     this.canvas.ontouchend = (evt) => {
       this.canvas.onmouseup();
     };
+
+    this.undoBtn.onclick = () => {
+      this.paths.pop();
+      this.#redraw();
+    };
   }
 
   #redraw = () => {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     draw.paths(this.ctx, this.paths);
+    if (this.paths.length > 0) {
+      this.undoBtn.disabled = false;
+    } else {
+      this.undoBtn.disabled = true;
+    }
   };
 
   #getMouse = (evt) => {
